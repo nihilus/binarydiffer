850bc453d0a0778567dd23611c9778ca755620ed
oh.jeongwook <oh.jeongwook@cddf30ce-1947-0410-93de-f5a27bb8667b>
2008-02-27 02:37:36 +0000

    Added items remotely
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDATracer.h
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDAUtils.cpp
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDAUtils.h
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\Database.h
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\dprintf.h
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDAAnalysis.cpp
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDAAnalysis.h
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDAAnalysisCommon.h
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDAGraph.cpp
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDAGraph.h
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IdaIncludes.h
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDAIPCParam.h
    T:\mat\Projects\ResearchTools\Binary\Static Analysis\Lib\IDATracer.cpp

---
+++ Lib/dprintf.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <ida.hpp>
+#include <idp.hpp>
+#include <expr.hpp>
+#include <bytes.hpp>
+#include <loader.hpp>
+#include <kernwin.hpp>
+#include <frame.hpp>
+#include <name.hpp>
+#include <bytes.hpp>
+#include <struct.hpp>
+#include <allins.hpp>
+#include <auto.hpp>
+
+#define dprintf msg
