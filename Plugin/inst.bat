885439d5219a1f44ac8a437b9bed260c5c8a370e
oh.jeongwook <oh.jeongwook@cddf30ce-1947-0410-93de-f5a27bb8667b>
2008-02-27 03:34:08 +0000

    fixed compilation problem

--- Plugin/inst.bat
+++ Plugin/inst.bat
@@ -1,4 +1,5 @@
 call "C:\Program Files\Microsoft Platform SDK\Setenv.cmd" /2000 /DEBUG
 nmake
+copy bin\DarunGrim2.plw ..\bin\
+rem "c:\Program Files\IDA\plugins" 
 pause
-copy bin\DarunGrim2.plw "c:\Program Files\IDA\plugins" 
